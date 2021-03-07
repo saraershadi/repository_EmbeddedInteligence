1d07cc80493e6fcee787a79624b2e246e9beccc8
Mahdiyousefi <Mahdiyousefi.irn@gmail.com>
2021-03-07 12:57:46 -0800

    Adding Code outlie and ducumation for c1m1 Assessment

--- stats.c
+++ stats.c
@@ -27,13 +27,9 @@
 /* Size of the Data Set */
 #define SIZE (40)
 
-void main() {
-
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
+void main()
+{
+	
 
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
