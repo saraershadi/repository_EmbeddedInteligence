fe81e137c0c613178b3c4cdc7b217e86771502ab
hojat929 <hojat.heydaripour@gmail.com>
2021-03-08 11:24:05 -0800

    first commit

--- stats.c
+++ stats.c
@@ -19,25 +19,16 @@
  *
  */
 
-
-
 #include <stdio.h>
-#include "stats.h"
-
-/* Size of the Data Set */
-#define SIZE (40)
-
-void main() {
-
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
-
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
-
+#include <conio.h>
+void main()
+{
+int list[40],max ;
+int i , j ;
+for(i=0 ; i<40 ; i++)
+{
+printf("\n enter moadel %d :",i+1);
+scanf("%f",&list[i]);
 }
-
+}
 /* Add other Implementation File Code Here */..
