155ba0feb32c1416b5a16de6ec4c14b224ea9700
hojat929 <hojat.heydaripour@gmail.com>
2021-03-09 02:02:11 -0800

    three  commit

--- stats.h
+++ stats.h
@@ -1,2 +1,7 @@
 #include <stdio.h>
-
+      printf("\n\nThe median is : %f\n",median);
+      printf("max: %d\n",max);                
+      printf("min: %d\n",min);   
+      printf("sum: %d\n",sum);
+      mean=sum/40;   
+      printf("mean: %1.f\n",mean);
