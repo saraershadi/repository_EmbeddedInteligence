6bc615b9889221d6a587aab52467c5a27319ee75
zeynab-sarvari <zeynab.sarvari@yahoo.com>
2021-03-10 14:17:06 -0800

    test

--- repository_EmbeddedInteligence/README.md
+++
@@ -1,1 +0,0 @@
-/* Add Author and Project Details here */
---
+++ repository_EmbeddedInteligence/stast.h
@@ -0,0 +1,12 @@
+#ifndef __STATS_H__
+#define __STATS_H__
+void print_statistics(int stats);
+
+void print_array(int* array, int size);
+int find_median(int* array, int size);
+
+int find_mean(int* array, int size);
+int find_maximum(int* array, int size);
+int find_minimum(int* array, int size);
+void sort_array(int* array, int size);
+#endif /* __STATS_H__ */
