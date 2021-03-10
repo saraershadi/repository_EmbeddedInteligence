f75889ed26725125f2817cf7372089cef4a5afad
zeynab-sarvari <zeynab.sarvari@yahoo.com>
2021-03-10 13:55:35 -0800

    zeynab sarvari

--- repository_EmbeddedInteligence/stats.c
+++ repository_EmbeddedInteligence/stats.c
@@ -1,30 +1,5 @@
-/******************************************************************************
- * Copyright (C) 2017 by Alex Fosdick - University of Colorado
- *...
- * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
- * permitted to modify this and use it to learn about the field of embedded
- * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
- *
- *****************************************************************************/
-/**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
- *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
- *
- */
-
-
-
 #include <stdio.h>
 #include "stats.h"
-
-/* Size of the Data Set */
 #define SIZE (40)
 
 void main() {
@@ -35,9 +10,112 @@ void main() {
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+  int test_int[SIZE],i;
+  for(i =0;i < SIZE;i++){
+    test_int[i] = (int)test[i];
+  }
+  
+  printf("The original array :\n");
+  print_array(test_int,SIZE);
+  sort_array(test_int, SIZE);
+  printf("The array after sorting process descendingly is : \n");
+  print_array(test_int, SIZE);
+  int T = find_maximum(test_int, SIZE);
+  printf("The Maximum Value in the array is : ");
+  print_statistics(T);
+  T = find_minimum(test_int, SIZE);
+  printf("The Minimum Value in the array is : ");
+  print_statistics(T);
+  T = find_median(test_int, SIZE);
+  printf("The Median Value of the array is : ");
+  print_statistics(T);
+  T = find_mean(test_int, SIZE);
+  printf("The Mean Value of the array is : ");
+  print_statistics(T);
 
 }
+void print_statistics(int stats){
+  printf("%d\n",stats);
+  for(int i = 1; i <= 60; i++){
+    printf("*");
+    if(i == 60 ){
+      printf("\n");
+    }
+  }
+}
+void print_array(int* array, int size){
+  int i;
+  for(i =0;i < size;i++){
+    if(i == 0){
+      printf("%d\t", *(array + i));
+      continue;
+    }
+    else if((i+1)%5 == 0){
+      printf("%d\n", *(array + i));
+      continue;
+    }
+    else{
+      printf("%d\t", *(array + i));
+    }
+  }
+  for(i = 1; i <= 60; i++){
+    printf("*");
+    if(i == 60 ){
+      printf("\n");
+    }
+  }
+}
+int find_median(int* array, int size){
+  int median;
+  median = (*(array+ ((size - 1)/2)) + *(array + (size/2)))/2;
+  return median;
+}
+int find_mean(int* array, int size){
+  int i;
+  int mean = 0;
+  for(i =0;i < size;i++){
+    mean += *(array + i);
+  }
+  mean = mean /size;
+  return mean;
+}
+int find_maximum(int* array, int size){
+  int max;
+  int i;
+  for(i =0;i < size;i++){
+    if(i == 0){
+      max = *(array + i);
+    }
+    if(*(array + i) > max){
+      max = *(array + i);
+    }
+  }
+  return max;
+}
+int find_minimum(int* array, int size){
+  int mini;
+  int i;
+  for(i = 0;i < size;i++){
+    if(i == 0){
+      mini = *(array + i);
+    }
+    if(*(array + i) < mini){
+      mini = *(array + i);
+    }
+  }
+  return mini;
+}
+void sort_array(int* array, int size){
+  int i,j;
+  for(i = 0; i < size; i++){
+    int temp;
+    for(j = i+1 ; j < size; j++){
+      if(*(array + i) < *(array + j)){
+        temp = *(array + i);
+        *(array + i) = *(array + j);
+        *(array + j) = temp;
+      }
+    }
+  }
+}
 
-/* Add other Implementation File Code Here */..
